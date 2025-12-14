ec197c848804fc9dfaf6bde7251b022d33be11b9
The-Quinnzite-Mushroom <quinn@trusler.net>
2025-12-13 17:15:19 -0800

    added player area

--- level_1.gd
+++ level_1.gd
@@ -3,4 +3,5 @@ extends Node2D
 const CURRENT_LEVEL_NUM = 1
 
 func go_to_next_level():
+	print("go to next level")
 	get_tree().change_scene_to_file("res://levels/level_"+str(CURRENT_LEVEL_NUM)+".tscn")
