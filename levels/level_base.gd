f5429efd21694af9296ea193651424633f9015dc
gnorris55 <graemenorris99@gmail.com>
2025-12-13 17:07:42 -0800

    added level base

---
+++ levels/level_base.gd
@@ -0,0 +1,16 @@
+extends Node
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+func go_to_next_level():
+	pass
+	
+func reload_level():
+	get_tree().reload_current_scene()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
