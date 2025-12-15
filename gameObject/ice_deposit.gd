c0d4eab35ea135e1678cf4e36fcc4891cc0dc024
gnorris55 <graemenorris99@gmail.com>
2025-12-14 16:57:34 -0800

    fixed minor issue

--- gameObject/ice_deposit.gd
+++ gameObject/ice_deposit.gd
@@ -17,8 +17,8 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		var ice_circle = area.get_node("./../iceCircle")
 		ice_circle.current_health += ice_circle.full_health / 3.0
 		
-		if ice_circle.current_health > ice_circle.max_health:
-			ice_circle.current_health = ice_circle.max_health
+		if ice_circle.current_health > ice_circle.full_health:
+			ice_circle.current_health = ice_circle.full_health
 			
 		
 		ice_circle.ice_bar.value = ice_circle.current_health
