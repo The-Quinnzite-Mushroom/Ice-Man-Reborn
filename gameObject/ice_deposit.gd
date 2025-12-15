7649fade0f635d45408e0302275c7c9c5b5dbfa4
The-Quinnzite-Mushroom <quinn@trusler.net>
2025-12-14 17:29:30 -0800

    Added pickup to tutorial

--- gameObject/ice_deposit.gd
+++ gameObject/ice_deposit.gd
@@ -5,10 +5,11 @@ extends Node2D
 func _ready() -> void:
 	pass # Replace with function body.
 
-
+var delta_total = 0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	delta_total += delta
+	rotation = sin(delta_total*3 + 1)
 
 
 func _on_area_2d_area_entered(area: Area2D) -> void:
